cc33b8c4e55b1ea3e86ae3251b8a8f72fd9d78b7
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2012-02-06 16:08:53 -0600

    Create GUI shell

---
+++ src/main/java/edu/vanderbilt/mc/biostat/tracker/App.java
@@ -0,0 +1,10 @@
+package edu.vanderbilt.mc.biostat.tracker;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Window win = new Window();
+        win.setVisible(true);
+    }
+}
